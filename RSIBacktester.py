4ce7d468f59d2a0e1bceda8651e7e6eebe7f3cb3
StevenvdHeijden <67513934+StevenvdHeijden@users.noreply.github.com>
2024-08-06 11:35:00 +0200

    Update RSIBacktester.py

--- RSIBacktester.py
+++ RSIBacktester.py
@@ -5,48 +5,52 @@ from scipy.optimize import brute
 plt.style.use("seaborn")
 import period_converter
 
-class RSIBacktester(): 
-    ''' Class for the vectorized backtesting of MACD-based trading strategies.
+class RSIBacktester:
+    ''' Class for the vectorized backtesting of RSI-based trading strategies.
 
     Attributes
     ==========
-    symbol: str
-        ticker symbol with which to work with
-    periods: int
-        time periods for Movering Averages RSI
-    start: str
-        start date for data retrieval
-    end: str
-        end date for data retrieval
-    month: str
-        the month to extract the data from
-    ti: str
-        time interval candlestick
-    tc: float
-        proportional transaction costs per trade
+    symbol : str
+        Ticker symbol to work with.
+    periods : int
+        Time periods for calculating the RSI.
+    rsi_lower : int
+        Lower bound for the RSI to signal a buy.
+    rsi_upper : int
+        Upper bound for the RSI to signal a sell.
+    start : str
+        Start date for data retrieval.
+    end : str
+        End date for data retrieval.
+    month : str
+        The month to extract the data from.
+    ti : str
+        Time interval for candlestick data.
+    tc : float
+        Proportional transaction costs per trade.
         
         
     Methods
     =======
     get_data:
-        retrieves and prepares the data
+        Retrieves and prepares the data.
         
     set_parameters:
-        sets one or two new SMA/EMA parameters
+        Sets new RSI parameters.
         
     test_strategy:
-        runs the backtest for the SMA/EMA-based strategy
+        Runs the backtest for the RSI-based strategy.
         
     plot_results:
-        plots the performance of the strategy compared to buy and hold
+        Plots the performance of the strategy compared to buy and hold.
         
     update_and_run:
-        updates EMA parameters and returns the negative absolute performance (for minimization algorithm)
+        Updates RSI parameters and returns the negative absolute performance (for minimization algorithm).
         
     optimize_parameters:
-        implements a brute force optimization for the two SAM/EMA parameters
+        Implements a brute force optimization for the RSI parameters.
     '''
-    
+
     def __init__(self, symbol, periods, rsi_lower, rsi_upper, start, end, month, ti, tc):
         self.symbol = symbol
         self.periods = periods
@@ -57,16 +61,16 @@ class RSIBacktester():
         self.month = month
         self.ti = ti
         self.tc = tc
-        self.results = None 
+        self.results = None
         self.get_data()
-        
+
     def __repr__(self):
-        return "RSIBacktester(symbol = {}, periods = {}, rsi_lower = {}, rsi_upper = {}, start = {}, end = {}, month = {})".format(self.symbol, self.periods, self.rsi_lower, self.rsi_upper, self.start, self.end, self.month)
-        
+        return f"RSIBacktester(symbol={self.symbol}, periods={self.periods}, rsi_lower={self.rsi_lower}, rsi_upper={self.rsi_upper}, start={self.start}, end={self.end}, month={self.month})"
+
     def get_data(self):
         ''' Retrieves and prepares the data.
         '''
-        column_names = ["Date","Time","Open","High","Low","Close","Volume"]
+        column_names = ["Date", "Time", "Open", "High", "Low", "Close", "Volume"]
         folder_path = f"C:\\Users\\steve\\AlgoTradingFrx\\csv files\\{self.symbol}\\HISTDATA_COM_MT_{self.symbol}_M12023{self.month}\\"
         raw = pd.read_csv(f"{folder_path}DAT_MT_{self.symbol}_M1_2023{self.month}.csv", sep=',', header=None)
         raw.columns = column_names
@@ -77,16 +81,21 @@ class RSIBacktester():
         raw.dropna(inplace=True)
 
         raw.rename(columns={"Close": "price"}, inplace=True)
+
+        # Calculate logarithmic returns
         raw["returns"] = np.log(raw.price / raw.price.shift(1))
+
+        # Calculate RSI
         raw["U"] = np.where(raw.price.diff() > 0, raw.price.diff(), 0)
         raw["D"] = np.where(raw.price.diff() < 0, raw.price.diff(), 0)
         raw["MA_U"] = raw.U.rolling(self.periods).mean()
         raw["MA_D"] = raw.D.rolling(self.periods).mean()
         raw["RSI"] = raw.MA_U / (raw.MA_U + raw.MA_D) * 100
+
         self.data = raw
-        
-    def set_parameters(self, periods = None, rsi_lower = None, rsi_upper = None):
-        ''' Updates RSI parameters and resp. time series.
+
+    def set_parameters(self, periods=None, rsi_lower=None, rsi_upper=None):
+        ''' Updates RSI parameters and respective time series.
         '''
         if periods is not None:
             self.periods = periods
@@ -94,12 +103,8 @@ class RSIBacktester():
             self.data["MA_D"] = self.data.D.rolling(self.periods).mean()
             self.data["RSI"] = self.data.MA_U / (self.data.MA_U + self.data.MA_D) * 100
 
-            self.rsi_lower = rsi_lower
-            self.rsi_upper = rsi_upper
-
         if rsi_lower is not None:
             self.rsi_lower = rsi_lower
-            self.rsi_upper = rsi_upper
 
         if rsi_upper is not None:
             self.rsi_upper = rsi_upper
@@ -114,24 +119,29 @@ class RSIBacktester():
         data["strategy"] = data["position"].shift(1) * data["returns"]
         data.dropna(inplace=True)
         
-        # determine when a trade takes place
+        # Determine when a trade takes place
         data["trades"] = data.position.diff().fillna(0).abs()
         
-        # subtract transaction costs from return when trade takes place
+        # Subtract transaction costs from return when trade takes place
         data.strategy = data.strategy - data.trades * self.tc
         
+        # Calculate cumulative returns
         data["creturns"] = data["returns"].cumsum().apply(np.exp)
         data["cstrategy"] = data["strategy"].cumsum().apply(np.exp)
+
         self.results = data
-        
+
         if not data.empty:
-            perf = data["cstrategy"].iloc[-1] # absolute performance of the strategy
-            outperf = perf - data["creturns"].iloc[-1] # out-/underperformance of strategy
+            # Absolute performance of the strategy
+            perf = data["cstrategy"].iloc[-1]
+            # Out-/underperformance of strategy
+            outperf = perf - data["creturns"].iloc[-1]
         else:
-            perf = 1 # absolute performance of the strategy
-            outperf = 0 # out-/underperformance of strategy
+            perf = 1  # Absolute performance of the strategy
+            outperf = 0  # Out-/underperformance of strategy
+        
         return round(perf, 6), round(outperf, 6)
-    
+
     def plot_results(self):
         ''' Plots the cumulative performance of the trading strategy
         compared to buy and hold.
@@ -139,29 +149,28 @@ class RSIBacktester():
         if self.results is None:
             print("No results to plot yet. Run a strategy.")
         else:
-            title = "{} | PERIODS = {} | RSI_LOWER = {} | RSI_UPPER = {} | TC = {}".format(self.symbol, self.periods, self.rsi_lower, self.rsi_upper, self.tc)
+            title = f"{self.symbol} | PERIODS = {self.periods} | RSI_LOWER = {self.rsi_lower} | RSI_UPPER = {self.rsi_upper} | TC = {self.tc}"
             self.results[["creturns", "cstrategy"]].plot(title=title, figsize=(12, 8))
-            plt.savefig(f'{self.month}_RSI_AUDJPY.png')
-            #plt.show()
-        
+            plt.savefig(f'{self.month}_RSI_{self.symbol}.png')
+
     def update_and_run(self, RSI):
         ''' Updates RSI parameters and returns the negative absolute performance (for minimization algorithm).
 
         Parameters
         ==========
-        RSI: tuple
+        RSI : tuple
             RSI parameter tuple
         '''
         self.set_parameters(int(RSI[0]), int(RSI[1]), int(RSI[2]))
         return -self.test_strategy()[0]
-    
+
     def optimize_parameters(self, periods_range, rsi_lower_range, rsi_upper_range):
         ''' Finds global maximum given the RSI parameter ranges.
 
         Parameters
         ==========
-        RSI_range: tuple
-            tuples of the form (start, end, step size)
+        periods_range, rsi_lower_range, rsi_upper_range : tuple
+            Tuples of the form (start, end, step size)
         '''
         opt = brute(self.update_and_run, (periods_range, rsi_lower_range, rsi_upper_range), finish=None)
         return opt, -self.update_and_run(opt)
