aa09a6eded5d0c55ed712ce967f1bff1d3e61876
StevenvdHeijden <67513934+StevenvdHeijden@users.noreply.github.com>
2024-08-06 11:37:46 +0200

    Update MACDBacktester.py

--- MACDBacktester.py
+++ MACDBacktester.py
@@ -5,52 +5,52 @@ from scipy.optimize import brute
 plt.style.use("seaborn")
 import period_converter
 
-class MACDBacktester(): 
+class MACDBacktester:
     ''' Class for the vectorized backtesting of MACD-based trading strategies.
 
     Attributes
     ==========
-    symbol: str
-        ticker symbol with which to work with
-    ema_s: int
-        time window in days for fast period ema
-    ema_l: int
-        time window in days for slow period ema
-    signal_mw: int
-        time window in days for signal ema
-    start: str
-        start date for data retrieval
-    end: str
-        end date for data retrieval
-    month: str
-        the month to extract the data from
-    ti: str
-        time interval candlestick
-    tc: float
-        proportional transaction costs per trade
+    symbol : str
+        Ticker symbol to work with.
+    ema_s : int
+        Time window for the fast period EMA.
+    ema_l : int
+        Time window for the slow period EMA.
+    signal_mw : int
+        Time window for the signal EMA.
+    start : str
+        Start date for data retrieval.
+    end : str
+        End date for data retrieval.
+    month : str
+        The month to extract the data from.
+    ti : str
+        Time interval for candlestick data.
+    tc : float
+        Proportional transaction costs per trade.
         
         
     Methods
     =======
     get_data:
-        retrieves and prepares the data
+        Retrieves and prepares the data.
         
     set_parameters:
-        sets one or two new SMA/EMA parameters
+        Sets new MACD parameters.
         
     test_strategy:
-        runs the backtest for the SMA/EMA-based strategy
+        Runs the backtest for the MACD-based strategy.
         
     plot_results:
-        plots the performance of the strategy compared to buy and hold
+        Plots the performance of the strategy compared to buy and hold.
         
     update_and_run:
-        updates EMA parameters and returns the negative absolute performance (for minimization algorithm)
+        Updates MACD parameters and returns the negative absolute performance (for minimization algorithm).
         
     optimize_parameters:
-        implements a brute force optimization for the two SAM/EMA parameters
+        Implements a brute force optimization for the MACD parameters.
     '''
-    
+
     def __init__(self, symbol, ema_s, ema_l, signal_mw, start, end, month, ti, tc):
         self.symbol = symbol
         self.ema_s = ema_s
@@ -61,16 +61,16 @@ class MACDBacktester():
         self.month = month
         self.ti = ti
         self.tc = tc
-        self.results = None 
+        self.results = None
         self.get_data()
-        
+
     def __repr__(self):
-        return "MACDBacktester(symbol = {}, ema_s = {}, ema_l = {}, signal_mw = {}, start = {}, end = {}, month = {})".format(self.symbol, self.ema_s, self.ema_l, self.signal_mw, self.start, self.end, self.month)
-        
+        return f"MACDBacktester(symbol={self.symbol}, ema_s={self.ema_s}, ema_l={self.ema_l}, signal_mw={self.signal_mw}, start={self.start}, end={self.end}, month={self.month})"
+
     def get_data(self):
         ''' Retrieves and prepares the data.
         '''
-        column_names = ["Date","Time","Open","High","Low","Close","Volume"]
+        column_names = ["Date", "Time", "Open", "High", "Low", "Close", "Volume"]
         folder_path = f"C:\\Users\\steve\\AlgoTradingFrx\\csv files\\{self.symbol}\\HISTDATA_COM_MT_{self.symbol}_M12023{self.month}\\"
         raw = pd.read_csv(f"{folder_path}DAT_MT_{self.symbol}_M1_2023{self.month}.csv", sep=',', header=None)
         raw.columns = column_names
@@ -81,31 +81,36 @@ class MACDBacktester():
         raw.dropna(inplace=True)
 
         raw.rename(columns={"Close": "price"}, inplace=True)
+
+        # Calculate logarithmic returns
         raw["returns"] = np.log(raw.price / raw.price.shift(1))
-        raw["EMA_S"] = raw["price"].ewm(span = self.ema_s, min_periods = self.ema_s).mean()
-        raw["EMA_L"] = raw["price"].ewm(span = self.ema_l, min_periods = self.ema_l).mean()
-        raw["signal_mw"] = raw["price"].ewm(span = self.signal_mw, min_periods = self.signal_mw).mean()
+
+        # Calculate MACD
+        raw["EMA_S"] = raw["price"].ewm(span=self.ema_s, min_periods=self.ema_s).mean()
+        raw["EMA_L"] = raw["price"].ewm(span=self.ema_l, min_periods=self.ema_l).mean()
+        raw["MACD"] = raw["EMA_S"] - raw["EMA_L"]
+        raw["MACD_Signal"] = raw["MACD"].ewm(span=self.signal_mw, min_periods=self.signal_mw).mean()
+
         self.data = raw
-        
-    def set_parameters(self, EMA_s = None, EMA_l = None, signal_mw = None):
-        ''' Updates MACD parameters and resp. time series.
+
+    def set_parameters(self, ema_s=None, ema_l=None, signal_mw=None):
+        ''' Updates MACD parameters and respective time series.
         '''
-        if EMA_s is not None:
-            self.ema_s = EMA_s
-            self.data["EMA_S"] = self.data["price"].ewm(span = self.ema_s, min_periods = self.ema_s).mean()
-            self.data["MACD"] = self.data.EMA_S - self.data.EMA_L
-            self.data["MACD_Signal"] = self.data.MACD.ewm(span = self.signal_mw, min_periods = self.signal_mw).mean()
+        if ema_s is not None:
+            self.ema_s = ema_s
+            self.data["EMA_S"] = self.data["price"].ewm(span=self.ema_s, min_periods=self.ema_s).mean()
         
-        if EMA_l is not None:
-            self.ema_l = EMA_l
-            self.data["EMA_L"] = self.data["price"].ewm(span = self.ema_l, min_periods = self.ema_l).mean()    
-            self.data["MACD"] = self.data.EMA_S - self.data.EMA_L
-            self.data["MACD_Signal"] = self.data.MACD.ewm(span = self.signal_mw, min_periods = self.signal_mw).mean()
+        if ema_l is not None:
+            self.ema_l = ema_l
+            self.data["EMA_L"] = self.data["price"].ewm(span=self.ema_l, min_periods=self.ema_l).mean()
+        
+        self.data["MACD"] = self.data["EMA_S"] - self.data["EMA_L"]
 
         if signal_mw is not None:
             self.signal_mw = signal_mw
-            self.data["MACD_Signal"] = self.data.MACD.ewm(span = self.signal_mw, min_periods = self.signal_mw).mean()     
-            
+        
+        self.data["MACD_Signal"] = self.data["MACD"].ewm(span=self.signal_mw, min_periods=self.signal_mw).mean()
+
     def test_strategy(self):
         ''' Backtests the trading strategy.
         '''
@@ -114,24 +119,29 @@ class MACDBacktester():
         data["strategy"] = data["position"].shift(1) * data["returns"]
         data.dropna(inplace=True)
         
-        # determine when a trade takes place
+        # Determine when a trade takes place
         data["trades"] = data.position.diff().fillna(0).abs()
         
-        # subtract transaction costs from return when trade takes place
+        # Subtract transaction costs from return when trade takes place
         data.strategy = data.strategy - data.trades * self.tc
         
+        # Calculate cumulative returns
         data["creturns"] = data["returns"].cumsum().apply(np.exp)
         data["cstrategy"] = data["strategy"].cumsum().apply(np.exp)
+
         self.results = data
-        
+
         if not data.empty:
-            perf = data["cstrategy"].iloc[-1] # absolute performance of the strategy
-            outperf = perf - data["creturns"].iloc[-1] # out-/underperformance of strategy
+            # Absolute performance of the strategy
+            perf = data["cstrategy"].iloc[-1]
+            # Out-/underperformance of strategy
+            outperf = perf - data["creturns"].iloc[-1]
         else:
-            perf = 1 # absolute performance of the strategy
-            outperf = 0 # out-/underperformance of strategy
+            perf = 1  # Absolute performance of the strategy
+            outperf = 0  # Out-/underperformance of strategy
+        
         return round(perf, 6), round(outperf, 6)
-    
+
     def plot_results(self):
         ''' Plots the cumulative performance of the trading strategy
         compared to buy and hold.
@@ -139,30 +149,28 @@ class MACDBacktester():
         if self.results is None:
             print("No results to plot yet. Run a strategy.")
         else:
-            title = "{} | EMA_S = {} | EMA_L = {} | SIGNAL_MW = {} | TC = {}".format(self.symbol, self.ema_s, self.ema_l, self.signal_mw, self.tc)
+            title = f"{self.symbol} | EMA_S = {self.ema_s} | EMA_L = {self.ema_l} | SIGNAL_MW = {self.signal_mw} | TC = {self.tc}"
             self.results[["creturns", "cstrategy"]].plot(title=title, figsize=(12, 8))
-            plt.savefig(f'{self.month}_MACD_AUDJPY.png')
-            #plt.show()
-        
+            plt.savefig(f'{self.month}_MACD_{self.symbol}.png')
+
     def update_and_run(self, MACD):
         ''' Updates MACD parameters and returns the negative absolute performance (for minimization algorithm).
 
         Parameters
         ==========
-        MACD: tuple
+        MACD : tuple
             MACD parameter tuple
         '''
         self.set_parameters(int(MACD[0]), int(MACD[1]), int(MACD[2]))
         return -self.test_strategy()[0]
-    
+
     def optimize_parameters(self, ema_s_range, ema_l_range, signal_mw_range):
         ''' Finds global maximum given the MACD parameter ranges.
 
         Parameters
         ==========
-        MACD_range: tuple
-            tuples of the form (start, end, step size)
+        ema_s_range, ema_l_range, signal_mw_range : tuple
+            Tuples of the form (start, end, step size)
         '''
         opt = brute(self.update_and_run, (ema_s_range, ema_l_range, signal_mw_range), finish=None)
         return opt, -self.update_and_run(opt)
-    
